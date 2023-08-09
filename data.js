efd9599da0a5854ef914de66b409aa28bd441b1a
mahou-anisphia <132260755+mahou-anisphia@users.noreply.github.com>
2023-08-09 20:00:48 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,66 @@
+import anisphiaHello from './assets/anisphiaHello.jfif';
+import anisphiaKeychain from './assets/anisphiaKeychain.jfif';
+import anisphiaSword from './assets/anisphiaSword.jfif';
+import euphieElegant from './assets/euphieElegant.jfif';
+import euphylliaKeychain from './assets/euphylliaKeychain.jfif';
+import euphylliaMahou from './assets/euphylliaMahou.jfif';
+import Illya from './assets/Illya.jfif';
+import shyEuphie from './assets/shyEuphie.jfif';
+import winkAnis from './assets/winkAnis.jfif';
+
+export const products = [
+    {
+        id: '1',
+        name: 'Anisphia Hello',
+        price: '$15.00',
+        image: anisphiaHello,
+    },
+    {
+        id: '2',
+        name: 'Anisphia Keychain',
+        price: '$8.00',
+        image: anisphiaKeychain,
+    },
+    {
+        id: '3',
+        name: 'Anisphia Sword',
+        price: '$20.00',
+        image: anisphiaSword,
+    },
+    {
+        id: '4',
+        name: 'Euphie Elegant',
+        price: '$18.00',
+        image: euphieElegant,
+    },
+    {
+        id: '5',
+        name: 'Euphyllia Keychain',
+        price: '$8.00',
+        image: euphylliaKeychain,
+    },
+    {
+        id: '6',
+        name: 'Euphyllia Mahou',
+        price: '$22.00',
+        image: euphylliaMahou,
+    },
+    {
+        id: '7',
+        name: 'Illya',
+        price: '$16.00',
+        image: Illya,
+    },
+    {
+        id: '8',
+        name: 'Shy Euphie',
+        price: '$19.00',
+        image: shyEuphie,
+    },
+    {
+        id: '9',
+        name: 'Wink Anis',
+        price: '$14.00',
+        image: winkAnis,
+    },
+];
